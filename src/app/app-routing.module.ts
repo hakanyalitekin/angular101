57b7fabb4f7c2186a43009c791862e54494a8c52
Hakan <hakanyalitekin@gmail.com>
2021-10-01 00:01:54 +0300

    data binding added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ import { RoleComponent } from './role/role.component';
 const routes: Routes = [
   {path:'home', loadChildren: () => import('./home/home.module').then(m=>m.HomeModule)},
   {path:'user', loadChildren: () => import('./user/user.module').then(m=>m.UserModule)},
+  {path:'data-binding', loadChildren: () => import('./data-binding/data-binding.module').then(m=>m.DataBindingModule)},
   {path:'role',component:RoleComponent },
-
   {path:'**',component:HomeComponent }
 ];
 
